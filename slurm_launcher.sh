4be75bb2065a2b602b07ebb4dec71941cb2ee060
elodiegermani1 <elodie.germani@irisa.fr>
2024-02-16 20:54:33 +0100

    Launcher

--- slurm_launcher.sh
+++ slurm_launcher.sh
@@ -6,9 +6,10 @@
 #SBATCH --cpus-per-task=16          # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
-#SBATCH --time=19:00:00              # maximum execution time (HH:MM:SS)
+#SBATCH --time=24:00:00              # maximum execution time (HH:MM:SS)
 #SBATCH --output=vae-train%j.out # output file name
 #SBATCH --error=vae-train%j.err  # error file name
+#SBATCH --qos=qos_gpu-t4
 
 source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv
 
