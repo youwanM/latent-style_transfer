a8e6b4e1fe453d06423276c0b842d34465d1e817
elodiegermani1 <elodie.germani@irisa.fr>
2024-02-22 12:48:13 +0100

    Change from sigmoid to tanh

--- slurm_launcher.sh
+++ slurm_launcher.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 #SBATCH --job-name=train-vae # nom du job
 #SBATCH --ntasks=1                   # number of MP tasks
-#SBATCH --partition=gpu_p2
+#SBATCH --partition=gpu_p13
 #SBATCH --ntasks=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:4                 # number of GPUs per node
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
-#SBATCH --time=19:00:00              # maximum execution time (HH:MM:SS)
+#SBATCH --time=2:00:00              # maximum execution time (HH:MM:SS)
 #SBATCH --output=vae-train%j.out # output file name
 #SBATCH --error=vae-train%j.err  # error file name
+#SBATCH --qos=qos_gpu-dev
 
 source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv
 
 /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u \
 /gpfswork/rech/gft/umh25bv/latent-style_transfer/main_vae.py \
 --data_dir data --dataset dataset_rh_4classes-jeanzay \
---model_save_dir ./vae_models-beta_1e-5 --batch_size 8 --epochs 200 --lr 1e-3 --beta 1e-5
+--model_save_dir ./vae_models-beta_1e-5-lr_1e-3 --batch_size 8 --epochs 200 --lr 1e-3 --beta 1e-5
 
 # /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/latent-style_transfer/main_ddpm.py \
 # --mode train --dataset dataset_rh_4classes-jeanzay \
