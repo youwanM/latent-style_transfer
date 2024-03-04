abc10afcf7128556297ed841082b3881ae31e5cb
elodiegermani1 <elodie.germani@irisa.fr>
2024-03-04 15:06:51 +0100

    Test with rfrh

--- slurm_launcher.sh
+++ slurm_launcher.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-#SBATCH --job-name=train_ddpm # nom du job
+#SBATCH --job-name=train_vae # nom du job
 #SBATCH --ntasks=1                   # number of MP tasks
-#SBATCH --partition=gpu_p2
+#SBATCH --partition=gpu_p13
 #SBATCH --ntasks=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:4                 # number of GPUs per node
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
-#SBATCH --time=24:00:00              # maximum execution time (HH:MM:SS)
-#SBATCH --output=train_ddpm%j.out # output file name
-#SBATCH --error=train_ddpm%j.err  # error file name
+#SBATCH --time=32:00:00              # maximum execution time (HH:MM:SS)
+#SBATCH --output=train_vae-rfrh%j.out # output file name
+#SBATCH --error=train_vae-rfrh%j.err  # error file name
 #SBATCH --qos=qos_gpu-t4
 
 source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv
 
-# /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u \
-# /gpfswork/rech/gft/umh25bv/latent-style_transfer/main_vae.py --mode train \
-# --data_dir data --dataset dataset_rh_4classes-jeanzay \
-# --model_save_dir ./vae_models-no_bias --batch_size 8 --epochs 200 --lr 1e-4 --beta 1e-5
-
 /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u \
-/gpfswork/rech/gft/umh25bv/latent-style_transfer/main_vae.py --mode test \
---data_dir data --dataset dataset_rh_4classes-jeanzay \
---model_save_dir ./vae_models --sample_save_dir ./vae_samples \
---test_iter 83
+/gpfswork/rech/gft/umh25bv/latent-style_transfer/main_vae.py --mode train \
+--data_dir data --dataset dataset_rhrh_4classes-jeanzay \
+--model_save_dir ./vae_models-rfrh --batch_size 8 --epochs 200 --lr 1e-4 --beta 1e-3
+
+# /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u \
+# /gpfswork/rech/gft/umh25bv/latent-style_transfer/main_vae.py --mode test \
+# --data_dir data --dataset dataset_rh_4classes-jeanzay \
+# --model_save_dir ./vae_models --sample_save_dir ./vae_samples \
+# --test_iter 83
 
 # /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/latent-style_transfer/main_ddpm.py \
 # --mode train --dataset dataset_rh_4classes-jeanzay \
